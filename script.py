31b0e0748fe0bb2bc3195e6ebac137eae52f2237
Z1P TI3 <z1pti3@outlook.com>
2020-10-06 19:43:30 +0100

    Fixed install bug

--- script.py
+++ script.py
@@ -6,14 +6,15 @@ class _script(plugin._plugin):
     def install(self):
         # Register models
         model.registerModel("script","_script","_action","plugins.script.models.action")
-        model.registerModel("scriptBlock","_scriptBlock","_trigger","plugins.script.models.action")
+        model.registerModel("scriptBlock","_scriptBlock","_action","plugins.script.models.action")
         return True
 
     def uninstall(self):
         # deregister models
         model.deregisterModel("script","_script","_action","plugins.script.models.action")
+        model.deregisterModel("scriptBlock","_scriptBlock","_action","plugins.script.models.action")
         return True
     
     def upgrade(self,LatestPluginVersion):
         if self.version < 1.2:
-            model.registerModel("scriptBlock","_scriptBlock","_trigger","plugins.script.models.action")
+            model.registerModel("scriptBlock","_scriptBlock","_action","plugins.script.models.action")
