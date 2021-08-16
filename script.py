3ab672c0cb04e2b0a356e78e8d7977b51af22f92
z1pti3 <z1pti3@outlook.com>
2021-08-16 18:09:15 +0000

    Released 4.0 and enabled backwards support

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 from core import plugin, model
 
+import jimi
+
 class _script(plugin._plugin):
-    version = 3.11
+    version = 4.0
 
     def install(self):
         # Register models
@@ -14,4 +16,12 @@ class _script(plugin._plugin):
         return True
     
     def upgrade(self,LatestPluginVersion):
-        pass
+        if self.version < 4.0:
+            from plugins.script.models import action
+            classes = jimi.db.getClassByName("_scriptBlock",None)
+            if len(classes) == 1:
+                scriptBlocks = action._scriptBlock().getAsClass(query={ "classID" : classes[0]["_id"] })
+                for scriptBlock in scriptBlocks:
+                    scriptBlock.scriptBlock = scriptBlock.scriptBlock.replace("def run(data):\r\n","def run(data):\r\n\tdata = data[\"flowData\"]\r\n").replace("def run(data):\n","def run(data):\n\tdata = data[\"flowData\"]\n")
+                    scriptBlock.update(["scriptBlock"])
+                
